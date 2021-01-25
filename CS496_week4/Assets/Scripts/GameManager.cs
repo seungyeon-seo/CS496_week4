2af185efae2668d398ad05a9c41e8b21186fbd21
seungyeon-seo <kcd6256@gmail.com>
2021-01-25 18:02:04 +0900

    implement respawn player

--- CS496_week4/Assets/Scripts/GameManager.cs
+++ CS496_week4/Assets/Scripts/GameManager.cs
@@ -33,12 +33,18 @@ public class GameManager : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        time += 1;
-        if (ballPrefab != null && time == 200)
+        switch (mapNumber)
         {
-            time = 0;
-            SpawnBall();
+            case 2:
+                time += 1;
+                if (time == 200)
+                {
+                    SpawnBall();
+                }
+                checkRespawn();
+                break;
         }
+        
     }
 
     private void SpawnPlayer()
@@ -62,13 +68,31 @@ public class GameManager : MonoBehaviourPunCallbacks
         PhotonNetwork.Instantiate(playerPrefab.name, initPosition, rotation);
     }
 
-    private void FixedUpdate()
+    void checkRespawn()
     {
-        
+        GameObject player = GameObject.Find(playerPrefab.name+"(Clone)");
+        if (player == null)
+            return;
+        Vector3 pos = player.transform.position;
+        if (pos.x < -6.95 || pos.x > 6.67 )
+        {
+            RespawnPlayer(player);
+        }
+        else if (pos.y < -2.19)
+        {
+            RespawnPlayer(player);
+        }
+    }
+
+    void RespawnPlayer(GameObject player)
+    {
+        player.SetActive(false);
+        SpawnPlayer();
     }
 
     void SpawnBall()
     {
+        time = 0; 
         Vector3 initPosition = new Vector3(Random.Range(-4, 3.8f), 17.34f, 51.89f);
         PhotonNetwork.Instantiate(ballPrefab.name, initPosition, new Quaternion(0, 0, 0, 0));
     }
